91a03179b36a56104431e3b5095a4e890e36d8b0
GT Tech <gtyagi.biz@gmail.com>
2019-03-26 20:38:58 +0000

    JacksonCustomDateDeserializer.java edited online with Bitbucket to add header

--- examples/mongodb-spring-data-querydsl-value-operators-example/src/main/java/org/bitbucket/gt_tech/spring/data/querydsl/value/operators/example/utils/JacksonCustomDateDeserializer.java
+++ examples/mongodb-spring-data-querydsl-value-operators-example/src/main/java/org/bitbucket/gt_tech/spring/data/querydsl/value/operators/example/utils/JacksonCustomDateDeserializer.java
@@ -1,4 +1,19 @@
-package org.bitbucket.gt_tech.spring.data.querydsl.value.operators.example.utils;
+/*******************************************************************************
+ * Copyright (c) 2019 @gt_tech
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+ package org.bitbucket.gt_tech.spring.data.querydsl.value.operators.example.utils;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,7 +25,11 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
+/**
+ * Custom Deserializer for Jackson for {@link Date}
+ *
+ * @author gt_tech 
+ */
 public class JacksonCustomDateDeserializer extends JsonDeserializer<Date>
 {
     @Override
